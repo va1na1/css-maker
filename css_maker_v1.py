6595416135f48a66b1d82f7a85fa9620950b860d
va1na1 <77720954+va1na1@users.noreply.github.com>
2021-05-20 09:16:16 +0530

    Add files via upload

---
+++ css_maker_v1.py
@@ -0,0 +1,60 @@
+#BYVA1NA1YT
+print(' Hello, im a css maker, i can do all the css work of krunker for you')
+start=str(input('Press any button to start:'))
+bgd=str(input("what color do you want the bottom buttons to be colored- "))
+btpx=str(input("Do you want the bottom buttons to be rounded or flat(type 24 for rounded and 4 for flat)?- "))
+mbg=str(input('what color do you want your side buttons to be- '))
+mbr=str(input('Do you want the side buttons to rounded or flat?(24 for round,4 for flat)-'))
+mlg=str(input('Put the link of what you want your main image to be here- '))
+font=str(input('What font do you want the text to be(theres a chance this may not work)?'))
+msbg=str(input('What color do you want the menus to be '))
+ovr=str(input('What is the colour theme you want for your css(colour)?'))
+
+
+print('.buttonPI {')
+print('    background-color: ',bgd,' !important;')
+print('    border-radius:',btpx+'px')
+print('}')
+print('.buttonG {')
+print('    background-color: ',bgd,' !important;')
+print('    border-radius:',btpx+'px')
+print('}')
+print('.buttonR {')
+print('    background-color: ',bgd,' !important;')
+print('    border-radius:',btpx+'px')
+print('}')
+print('.buttonO {')
+print('    background-color: ',bgd,' !important;')
+print('    border-radius:',btpx+'px')
+print('}')
+print('.buttonP {')
+print('    background-color: ',bgd,' !important;')
+print('    border-radius:',btpx+'px')
+print('}')
+print('.menuItem {background-color: ',mbg,'}')
+print('#mainLogo {')
+print('    content: url(',mlg,');')
+print('}')
+print('#menuWindow.dark {background-color: ',msbg,';')
+print('}')
+print('.setHed {background-color:',msbg,'}')
+print('.setBodH {background-color:',msbg,'}')
+print('.settingTab {background-color:',msbg,'}')
+print('.settingsHeader {background-color:',msbg,'}')
+print('.headerBar {background-color:',ovr,'}')
+print('#termsInfo {background-color:',ovr,'}')
+print('#newsHolder  {background-color:',ovr,'}')
+print('#streamContainer {background-color:',ovr,'}')
+print('#mapInfo {background-color:',ovr,'}')
+print('.mFeatSepar {background-color:',ovr,'}')
+print('    * {font-family:',font,'}')
+
+
+
+
+
+
+
+
+
+start=str(input('Press any button to continue'))
